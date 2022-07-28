62505662f51a8977f99633e1c7e536a1e3791894
kimgeonmin <kimgunmin77@gmail.com>
2022-07-29 02:32:37 +0900

    title, date 수집 방식 변경

--- Summer_Data_Study/naver_news_detail.py
+++ Summer_Data_Study/naver_news_detail.py
@@ -4,8 +4,17 @@ import pandas as pd
 
 l = []
 
-#headers
+# headers
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'}
+
+
+def basic_info(li):
+    url = li.a['href']
+    for t in li.select('#main_content > div > ul > li > dl > dt > a'):
+        title = t.text.strip()
+    date = li.select_one('#main_content > div > ul > li > dl > dd > span.date').text.strip()
+
+    return title, date, url
 
 def detail_info(url):
     res = requests.get(url, headers = headers)
@@ -14,23 +23,7 @@ def detail_info(url):
 
     soup = BeautifulSoup(text, 'html.parser')
 
-    #title
-    try :
-        title = soup.select_one('#ct > div > div > h2').text.strip()
-    except:
-        title = soup.select_one('#content > div > div > div > div > h4').text.strip()
-    #date
-    try:
-        date = soup.select_one('#ct > div > div > div > div > span').text.strip()
-    except:
-        date = soup.select_one('#content > div > div> div > div> div > span').text.strip()
-        if date == '새로운 뉴스':
-            date = soup.select_one('#content > div.end_ct > div > div.article_info > span > em').text.strip()
-        else:
-            list(date)
-            date = date[5:]
-            str(date)
-    #contents
+    # contents
     try:
         contents = soup.select_one('#dic_area').text.strip()
     except:
@@ -39,25 +32,26 @@ def detail_info(url):
         except:
             contents = soup.select_one('#content > div.end_ct > div > div.end_body_wrp').text.strip()
 
-    return title, date, contents
+    return contents
 
-#날짜 리스트
+# 날짜 리스트 만들기
 day_url = []
 for day in range(30):
     day_url.append('https://news.naver.com/main/list.naver?mode=LS2D&mid=shm&sid2=229&sid1=105&date='+ str(20220601 + day))
 
+# 메인 반복문
 for day in day_url:
     res = requests.get(day, headers=headers)
     text = res.text
 
     soup = BeautifulSoup(text, 'html.parser')
 
-    #탐색할 페이지 리스트
+    # 탐색할 페이지 리스트 만들기
     page = [day]
     for li in soup.select('#main_content > div.paging > a'):
         page.append('https://news.naver.com/main/list.naver' + li['href'])
 
-    #페이지 탐색
+    # 페이지 탐색
     for li in reversed(page):
         res = requests.get(li, headers=headers)
         text = res.text
@@ -65,12 +59,12 @@ for day in day_url:
         soup = BeautifulSoup(text, 'html.parser')
 
         for li in reversed(soup.select('#main_content > div > ul > li')):
-            url = li.a['href']
+            title, date, url = basic_info(li)
 
-            title, date, contents = detail_info(url)
-            #print(f'Title : \n{title}')
+            contents = detail_info(url)
+            ##print(f'Title : \n{title}')
             print(f'Date : \n{date}')
-            #print(f'Contents : \n{contents}')
+            ##print(f'Contents : \n{contents}')
             l.append([title, date, contents])
 
 df = pd.DataFrame(l, columns = ['title', 'url', 'contents'])
