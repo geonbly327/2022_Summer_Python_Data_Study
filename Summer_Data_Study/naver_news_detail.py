2356b6d2bc486ce55f1d271ebe46dbe404958611
kimgeonmin <kimgunmin77@gmail.com>
2022-07-31 17:14:47 +0900

    MySQL db에 수집한 뉴스 data 넣기

--- Summer_Data_Study/naver_news_detail.py
+++ Summer_Data_Study/naver_news_detail.py
@@ -2,16 +2,22 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import pymysql
+import time
 import mysql_user_info # MySQL 정보
 
+start_time = time.time()
+
+# 뉴스의 기본 정보 수집
 def basic_info(li):
     url = li.a['href']
     for t in li.select('#main_content > div > ul > li > dl > dt > a'):
         title = t.text.strip()
     date = li.select_one('#main_content > div > ul > li > dl > dd > span.date').text.strip()
+    publisher = li.select_one('#main_content > div > ul > li > dl > dd > span.writing').text.strip()
 
-    return title, date, url
+    return title, date, publisher, url
 
+# 뉴스 기사 내에서 세부정보 수집
 def detail_info(url):
     res = requests.get(url, headers = headers)
     text = res.text
@@ -30,12 +36,20 @@ def detail_info(url):
 
     return contents
 
-# MySQL 연결
-user = mysql_user_info.user_info
-db = pymysql.connect(db=user['db'], host=user['host'], user=user['user'], passwd=user['passwd'], port=user['port'], charset=user['charset'])
-cursor = db.cursor(pymysql.cursors.DictCursor)
+# MySQL db에 data 넣기
+def insert_data(publisher, title, date):
+    user = mysql_user_info.user_info
+    db = pymysql.connect(db=user['db'], host=user['host'], user=user['user'], passwd=user['passwd'],
+                         port=user['port'], charset=user['charset'])
 
-l = []
+    sql = 'INSERT INTO news (publisher, title, date) VALUES (%s, %s, %s)'
+
+    with db:
+        with db.cursor() as cursor:
+            cursor.execute(sql, (publisher, title, date))
+            db.commit()
+
+##l = []
 
 # headers
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'}
@@ -65,13 +79,19 @@ for day in day_url:
         soup = BeautifulSoup(text, 'html.parser')
 
         for li in reversed(soup.select('#main_content > div > ul > li')):
-            title, date, url = basic_info(li)
+            title, date, publisher, url = basic_info(li)
+            ##contents = detail_info(url)
 
-            contents = detail_info(url)
             ##print(f'Title : \n{title}')
             print(f'Date : \n{date}')
-            ##print(f'Contents : \n{contents}')
-            l.append([title, date, contents])
 
-df = pd.DataFrame(l, columns = ['title', 'url', 'contents'])
-df.to_excel('naver_news.xlsx', index = False)
+            insert_data(publisher, title, date)
+
+            ##print(f'Contents : \n{contents}')
+            ##l.append([title, date, contents])
+
+##df = pd.DataFrame(l, columns = ['title', 'url', 'contents'])
+##df.to_excel('naver_news.xlsx', index = False)
+
+# 실행 시간
+print(f'Time : {time.time() - start_time}')
