7cd361384f03bffd6b20cc7f8716e068f93088fd
GBLY327 <kimgunmin77@gmail.com>
2022-08-20 15:53:03 +0900

    뉴스 제목, 본문에서 명사, 형용사 추출

--- Summer_Data_Study/news_data_analyze.py
+++ Summer_Data_Study/news_data_analyze.py
@@ -19,32 +19,47 @@ def fetch():
     return data
 
 # morpheme 테이블에 data 넣기
-def insert_data(id, type, word):
-    with pymysql.connect(db=user['db'], host=user['host'], user=user['user'], passwd=user['passwd'], port=user['port'], charset=user['charset']) as db:
-        with db.cursor() as cursor:
-            sql = 'INSERT INTO morpheme (id, type, word) VALUES (%s, %s, %s)'
-            cursor.execute(sql, (id, type, word))
-            db.commit()
+def insert_data(id, type, word, sort):
+    try:
+        with pymysql.connect(db=user['db'], host=user['host'], user=user['user'], passwd=user['passwd'], port=user['port'], charset=user['charset']) as db:
+            with db.cursor() as cursor:
+                sql = 'INSERT INTO ' + sort + '_morpheme (id, type, word) VALUES (%s, %s, %s)'
+                cursor.execute(sql, (id, type, word))
+                db.commit()
+    except:
+        pass
 
+# data fetch
 user = mysql_user_info.user_info
 data = fetch()
 
 # Okt 객체 선언
 okt = Okt()
 
-for i in range(25854):
-    id = data[i]['publisher'] + '-' + data[i]['date']
-    pos = okt.pos(data[i]['title'])
-    noun = okt.phrases((data[i]['title']))
+# main
+for i in data:
+    # id 생성
+    id = i['publisher'] + '-' + i['date']
+    print(id)
 
-    for k in pos:
+    # 형태소 분석
+    title_pos = okt.pos(i['title'])
+    title_noun = okt.phrases((i['title']))
+    body_pos = okt.pos(i['body'])
+    body_noun = okt.phrases((i['body']))
+
+    # 제목 형태소 분석을 db에 넣기
+    for k in title_noun:
+        insert_data(id, 'noun', k, 'title')
+
+    # 본문 형태소 분석을 db에 넣기
+    for k in body_noun:
+        insert_data(id, 'noun', k, 'body')
+    for k in body_pos:
         if k[1] == 'adjective':
-            insert_data(id, 'adjective', k[0])
-        else :
+            insert_data(id, 'adjective', k[0], 'body')
+        else:
             continue
-
-    for k in noun:
-        insert_data(id, 'noun', k)
 
 # 실행 시간
 print(f'Time : {time.time() - start_time}')
